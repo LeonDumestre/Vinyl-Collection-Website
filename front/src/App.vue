edefdfb39d3972b037ac21cda0e6da6ed1e66147
DUMESTRE LEON p2006532 <leon.dumestre@etu.univ-lyon1.fr>
2022-03-18 14:29:35 +0100

    Ajout de la correction de la séance 2 (front)

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,58 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <h1>Mes Taches</h1>
+    <form @submit.prevent="addTask">
+      <input type="text" placeholder="Text" v-model="value" />
+      <button type="submit">Ajouter une tache</button>
+    </form>
+    <Task
+        v-for="(task, index) in tasks"
+        :key="index"
+        :task="task"
+        @toggle="toggleCheck(task)"
+        @delete="deleteTask(index)"
+    />
+    <h2>Stats</h2>
+    <p>{{ done }}/{{ total }}({{ avg }}%)</p>
+  </div>
 </template>
+
+<script>
+import Task from './components/Task.vue';
+export default {
+  name: 'App',
+  components: {
+    Task
+  },
+  data: () => ({
+    value: '',
+    tasks: [],
+  }),
+  computed: {
+    total() {
+      return this.tasks.length;
+    },
+    done() {
+      return this.tasks.filter(task => task.checked).length
+    },
+    avg() {
+      return (this.total > 0 ? this.done / this.total * 100 : 0).toFixed(2);
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({ name: this.value, checked: false });
+      this.value = '';
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    toogleCheck(task) {
+      task.checked = !task.checked;
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -16,6 +61,11 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 50vw;
+  margin: auto;
   margin-top: 60px;
+}
+.task {
+  display: flex;
 }
 </style>
---
+++ front/src/components/Task.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="task">
+    <img :src="source" @click="toogleCheck" />
+    <p @click="deleteTask" :class="{ '-checked': task.checked }">{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+import Unchecked from '../assets/unchecked.svg'
+import Checked from '../assets/check.svg'
+export default {
+  name: 'Task',
+  props: {
+    task: Object
+  },
+  computed: {
+    source() {
+      return this.task.checked ? Checked : Unchecked;
+    }
+  },
+  methods: {
+    toogleCheck() {
+      this.$emit('toggle');
+    },
+    deleteTask() {
+      this.$emit('delete');
+    },
+  }
+}
+</script>
+
+
+<style scoped>
+.-checked {
+  text-decoration: line-through;
+}
+</style>
