fc332683d35f8fac28985dcdd51c78b35f48ad07
DUMESTRE LEON p2006532 <leon.dumestre@etu.univ-lyon1.fr>
2022-03-23 16:47:04 +0100

    Amélioration style Add Form

--- front/src/pages/Add.vue
+++ front/src/pages/Add.vue
@@ -5,23 +5,45 @@
 
       <div class="row">
         <h4>Informations du vinyle</h4>
-        <input type="text" placeholder="Nom de l'album" v-model="name" required />
-        <input type="file" required />
-        <input type="text" placeholder="Nom de l'artiste / groupe" v-model="artist" required />
-        <input type="text" placeholder="Label de l'album" v-model="label" required />
-        <input type="date" placeholder="Date de sortie" v-model="release_date" required />
-        <input type="number" placeholder="Prix à la sortie" v-model="release_price" required />
-        <input type="number" placeholder="Prix actuel" v-model="current_price" />
+        <input type="text" class="input-group" placeholder="Nom de l'album" v-model="name" required />
+        <input type="text" class="input-group" placeholder="Nom de l'artiste / groupe" v-model="artist" required />
+        <input type="text" class="input-group" placeholder="Label de l'album" v-model="label" required />
+        <input type="file" class="input-group" accept=".jpg" required />
       </div>
 
       <div class="row">
-        <h4>Acquisition</h4>
-        <input type="date" placeholder="Date d'acquisition" v-model="purchase_date" />
-        <input type="number" placeholder="Prix d'acquisition" v-model="current_price" />
-        <input type="number" placeholder="Quantité" v-model="quantity" required />
+        <div class="col-half">
+          <h4>Date de sortie</h4>
+          <div class="input-group">
+            <div class="col-third"><input type="number" placeholder="MM" min="1" max="12" v-model="month_release_date" /></div>
+            <div class="col-third"><input type="number" placeholder="YYYY" v-model="year_release_date" /></div>
+          </div>
+        </div>
+
+        <div class="col-half">
+          <h4>Prix à la sortie / actuel</h4>
+          <div class="input-group">
+            <div class="col-third"><input type="number" class="input-group" placeholder="Prix à la sortie" v-model="release_price" /></div>
+            <div class="col-third"><input type="number" class="input-group" placeholder="Prix actuel" v-model="current_price" /></div>
+          </div>
+        </div>
       </div>
 
-      <button type="submit">Ajouter le vinyle</button>
+      <div class="row">
+        <div class="col-half">
+          <h4>Date d'acquisition</h4>
+          <div class="input-group">
+            <div class="col-third"><input type="number" placeholder="MM" min="1" max="12" v-model="month_purchase_date" /></div>
+            <div class="col-third"><input type="number" placeholder="AAAA"  v-model="year_purchase_date" /></div>
+          </div>
+        </div>
+        <div class="col-half">
+          <h4>Quantité</h4>
+          <div class="col-third"><input type="number" min="0" max="999" v-model="quantity" /></div>
+        </div>
+      </div>
+
+      <button type="submit" class="btn btn-theme">Ajouter le vinyle</button>
     </form>
   </div>
 
@@ -40,8 +62,10 @@ export default {
     name: '',
     artist: '',
     label: '',
-    release_date: 0,
-    purchase_date: 0,
+    month_release_date: '',
+    year_release_date: '',
+    month_purchase_date: '',
+    year_purchase_date: '',
     release_price: 0,
     current_price: 0,
     purchase_price: 0,
