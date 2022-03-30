9d61cea393321af1b1547187cbaabad694968f2b
GERMAIN William <william.germain@etu.univ-lyon1.fr>
2022-03-30 14:04:55 +0200

    ajout d'un User

--- back/database.js
+++ back/database.js
@@ -66,5 +66,25 @@ export const Track = sequelize.define('Track', {
     }
 });
 
+export const User = sequelize.define('User', {
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
 Vinyl.sync();
 Track.sync();
+User.sync();
